fd6b4ea2a95b34f505ec42527637e9b6827cf573
sandresbal <s.andresbal@gmail.com>
2019-06-22 19:40:01 +0200

    changes db data

--- config/database.php
+++ config/database.php
@@ -40,7 +40,7 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
-        'mysql' => [
+        /*'mysql' => [
             'engine' => 'InnoDB',
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
@@ -58,6 +58,28 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
+        ],*/
+
+        'mysql' => [
+            'driver' => 'mysql',
+            'host' => 'db4free.net',
+            'port' => '3306',
+            'database' => 'trabajodaw',
+            'username' => 'sandresbalsera',
+            'password' => 'AtynVFKpD2z44xp',
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+            'modes'=>[
+                ‘ONLY_FULL_GROUP_BY’,
+                ‘STRICT_TRANS_TABLES’,
+                ‘NO_ZERO_IN_DATE’,
+                ‘NO_ZERO_DATE’,
+                ‘ERROR_FOR_DIVISION_BY_ZERO’,
+                ‘NO_ENGINE_SUBSTITUTION’,
+                ],
         ],
 
         'pgsql' => [
